201aa8c4a9d22732ec9b10a155f7103d7af8b252
Chuck Freitas <chuck@invisionapp.com>
2016-06-30 13:57:52 -0700

    fixes from Code review, indentation, removed unused test, cleaned up console.log

--- test/unit/lib/deploymentizer.spec.js
+++ test/unit/lib/deploymentizer.spec.js
@@ -17,6 +17,12 @@ describe("Deploymentizer", () =>  {
         const deploymentizer = new Deploymentizer(options);
         yield deploymentizer.loadConf();
         console.log(deploymentizer.paths);
+        expect(deploymentizer.paths.base).to.equal("/test/fixture");
+        expect(deploymentizer.paths.output).to.equal("/generated");
+        expect(deploymentizer.paths.cluster).to.equal("./clusters");
+        expect(deploymentizer.paths.images).to.equal("./manifests/images");
+        expect(deploymentizer.paths.type).to.equal("./type");
+        expect(deploymentizer.paths.resources).to.equal("./resources");
         done();
       })().catch( (err) => {
         done(err);
