898f83cb83e1ca01e20a74efbdf15e3d8750184f
Chuck Freitas <chuck@invisionapp.com>
2016-07-25 23:19:49 +0200

    fix for the shell script and test

---
+++ test/functional/deploymentizer.spec.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const expect = require("chai").expect;
+const exec = require('child_process').exec;
+
+describe("Deploymentizer", () => {
+	describe("shell script", () => {
+		it("should run successfully", (done) => {
+
+			process.env.SECRET_USERNAME = "myusername";
+			process.env.SECRET_PASSWORD = "mypassword";
+			process.env.GITHUB_TOKEN = "s@mpler@ndomt0ken";
+
+			var cmd = '/src/deploymentizer --conf="/test/fixture/kit.yaml"';
+
+			exec(cmd, function(error, stdout, stderr) {
+				console.log(`stdout: ${stdout}`);
+				console.log(`stderr: ${stderr}`);
+				if (error !== null) {
+					console.log(`exec error: ${error}`);
+					done(error);
+				} else {
+					done();
+				}
+			});
+		});
+	});
+});
